3a49d31ed59c7d04adff984aff3f07ed4b8305d0
icmor <54549777+icmor@users.noreply.github.com>
2020-12-12 17:13:48 -0500

    Changed name to vanilla

---
+++ emacs/vanilla/early-init.el
@@ -0,0 +1,6 @@
+;;;; Frame Configuration
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(set-face-attribute 'default nil :height 160)
