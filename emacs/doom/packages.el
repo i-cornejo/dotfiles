7b896ef88633e7cbb65227da1c273e560636a362
icmor <54549777+icmor@users.noreply.github.com>
2020-12-15 10:17:03 -0500

    Remove extraneous packages

--- emacs/doom/packages.el
+++ emacs/doom/packages.el
@@ -3,6 +3,10 @@
 
 (package! org-drill)
 
+(disable-packages! cuda-mode opencl-mode glsl-mode company-glsl)
+
+(disable-packages! lsp-python-ms anaconda-mode)
+
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
