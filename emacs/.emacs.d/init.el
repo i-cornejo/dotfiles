9181af75b91712aa79b09d091a1bbcd851e1ec62
icmor <54549777+icmor@users.noreply.github.com>
2020-12-18 23:38:42 -0500

    Major changes in package loading
    Focused on lazy loading and speed. Got startup time down to
    0.30s and discovered strange use-package bug.
    [bug](https://github.com/jwiegley/use-package/issues/895)

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -19,7 +19,6 @@
 (require 'bind-key)
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
-(use-package diminish)
 
 (setq byte-compile-warnings '(cl-functions)) ; package cl is deprecated warning
 
@@ -35,37 +34,34 @@
 ;;; Help
 
 (use-package which-key
-  :diminish
   :config
   (setq which-key-idle-delay 0.5)
   (which-key-mode))
 
 (use-package helpful
-  :after counsel
   :bind
   ("C-h f" . helpful-callable)
   ("C-h v" . helpful-variable)
   ("C-h k" . helpful-key)
   :hook
-  (emacs-lisp-mode . (lambda () (local-set-key (kbd "C-c C-d") #'helpful-at-point)))
+  (emacs-lisp-mode . (lambda ()
+		       (local-set-key (kbd "C-c C-d") #'helpful-at-point)))
   :config
   (setq counsel-describe-function-function #'helpful-callable
 	counsel-describe-variable-function #'helpful-variable))
 
 (use-package elisp-demos
-  :after helpful
-  :config
-  (advice-add 'helpful-update :after #'elisp-demos-advice-helpful-update))
+  :hook
+  (helpful-mode . (lambda ()
+		    (advice-add 'helpful-update
+				:after #'elisp-demos-advice-helpful-update))))
 
 ;;; General Completion
 
 (use-package ivy
-  :diminish
-  :init
-  (ivy-mode 1)
-  :bind
-  ("C-c C-r" . ivy-resume)
+  :defer t
   :config
+  (ivy-mode)
   (setq ivy-use-virtual-buffers t
 	enable-recursive-minibuffers t
 	ivy-read-action-function #'ivy-hydra-read-action
@@ -75,14 +71,15 @@
 	  (t . ivy--regex-fuzzy))))
 
 (use-package counsel
-  :diminish
   :bind
+  ("C-x C-f" . counsel-find-file)
+  ("C-x r b" . counsel-bookmark)
+  ("M-x" . counsel-M-x)
   ("C-c j j" . counsel-rg)
   ("C-c j i" . counsel-imenu)
   ("C-c j l" . counsel-find-library)
-  :init
-  (counsel-mode 1)
   :config
+  (counsel-mode)
   (ivy-configure 'counsel-imenu
     :update-fn 'auto))
 
@@ -90,14 +87,9 @@
   :bind
   ("C-s" . swiper-isearch))
 
-(use-package ivy-hydra
-  :after ivy)
-
-(use-package flx
-  :after ivy)
-
-(use-package amx
-  :after ivy)
+(use-package ivy-hydra :defer t)
+(use-package flx :defer t)
+(use-package amx :defer t)
 
 ;;; Window Management
 
@@ -112,12 +104,11 @@
 	aw-reverse-frame-list t
 	ace-window-display-mode t))
 
-(winner-mode 1)
+(winner-mode)
 
 ;;; Utilities
 
 (use-package gcmh
-  :diminish
   :hook
   (after-init . gcmh-mode))
 
@@ -131,14 +122,13 @@
   (pdf-loader-install))
 
 (use-package vterm
-  :bind*
-  ([f2] . vterm-toggle)
   :bind
   (:map vterm-mode-map
 	("C-u" . vterm--self-insert)))
   
 (use-package vterm-toggle
-  :defer t
+  :bind*
+  ([f2] . vterm-toggle)
   :config
   (setq vterm-toggle-hide-method 'reset-window-configration)
   (defun vterm-counsel-yank-pop-action (orig-fun &rest args)
@@ -155,7 +145,6 @@
 
 (use-package org
   :pin org
-  :ensure org-plus-contrib
   :hook
   (org-mode . visual-line-mode)
   :bind
@@ -172,11 +161,12 @@
 	org-archive-location "~/org/gtd/inbox.org::"
 	org-log-into-drawer t
 	org-refile-targets (quote ((nil :maxlevel . 3)
-                                   (org-agenda-files :maxlevel . 3)))
+                                   (org-agenda-files :maxlevel . 3)
+				   ("~/org/roam/notes/UNAM" :tag . "UNAM")))
 	org-agenda-custom-commands
 	'(("d" "Show scheduled study drills."agenda ""
 	   ((org-agenda-files
-	     (directory-files-recursively "~/Dropbox/learn/" org-agenda-file-regexp))
+	     (directory-files-recursively "~/org/roam/notes/" org-agenda-file-regexp))
 	    (org-agenda-entry-types '(:scheduled))
 	    (org-agenda-span 'week)))))
 
@@ -184,29 +174,50 @@
   (setq org-default-notes-file "~/org/gtd/inbox.org"
 	org-capture-templates
 	'(("i" "Inbox" entry (file "")
-	   "* TODO %?\n %i\n")
+	   "* TODO %?\n")
 	  ("h" "Homework" entry (file+headline "~/org/gtd/gtd.org" "Homework")
-	   "* TODO %? :hw:\n %i\n")
+	   "* TODO %? :hw:\n")
 	  ("t" "Tasks" entry (file+headline "~/org/gtd/gtd.org" "Tasks")
-	   "* TODO %?\n %i\n")
-	  ("v" "Vocabulary" entry (file "~/org/zettel/notes/vocabulary"))))
+	   "* TODO %?\n")
+	  ("m" "Matemáticas" entry (file+headline "~/org/roam/notes/unam.org" "Matemáticas")
+	   "* TODO %^{Todo} :drill:\n%?\n")
+	  ("e" "Español" entry (file+headline "~/org/roam/notes/unam.org" "Español")
+	   "* TODO %^{Todo} :drill:\n%?\n")
+	  ("f" "Física" entry (file+headline "~/org/roam/notes/unam.org" "Física")
+	   "* TODO %^{Todo} :drill:\n%?\n")
+	  ("q" "Química" entry (file+headline "~/org/roam/notes/unam.org" "Química")
+	   "* TODO %^{Todo} :drill:\n%?\n")
+	  ("b" "Biología" entry (file+headline "~/org/roam/notes/unam.org" "Biología")
+	   "* TODO %^{Todo} :drill:\n%?\n")
+	  ("h" "Historia" entry (file+headline "~/org/roam/notes/unam.org" "Historia Universal")
+	   "* TODO %^{Todo} :drill:\n%?\n")
+	  ("m" "México" entry (file+headline "~/org/roam/notes/unam.org" "Historia de México")
+	   "* TODO %^{Todo} :drill:\n%?\n")
+	  ("x" "Literatura" entry (file+headline "~/org/roam/notes/unam.org" "Literatura")
+	   "* TODO %^{Todo} :drill:\n%?\n")
+	  ("g" "Geografía" entry (file+headline "~/org/roam/notes/unam.org" "Geografía")
+	   "* TODO %^{Todo} :drill:\n%?\n")))
 
   ;; Extra Org modules
   (add-to-list 'org-modules  'org-habit t))
 
-(use-package org-drill :after org)
+(use-package org-plus-contrib
+  :defer t)
+
+(use-package org-drill
+  :commands
+  (org-drill))
+
 (use-package org-bullets
   :hook
-  (org-mode . (lambda () (org-bullets-mode 1))))
+  (org-mode . org-bullets-mode))
 
 ;;; Org Roam
 
 (use-package org-roam
-  :after org
   :bind
   ("C-c n n" . org-roam-jump-to-index)
   ("C-c n f" . org-roam-find-file)
-  :diminish
   :config
   (setq org-roam-directory "~/org/roam")
   (org-roam-mode)
@@ -250,37 +261,37 @@
       show-paren-delay  0
       show-paren-style 'mixed)
 (show-paren-mode)
+
 (setq-default fill-column 80)
 (add-hook 'prog-mode-hook 'auto-fill-mode)
 (add-hook 'prog-mode-hook 'electric-pair-local-mode)
-(add-hook 'after-init-hook  'global-auto-revert-mode)
 
 (use-package ws-butler
-  :diminish
   :hook
   (prog-mode . ws-butler-mode)
   (markdown-mode . ws-butler-mode)
   (org-mode . ws-butler-mode))
 
 (use-package company
-  :diminish
   :hook
-  (prog-mode . global-company-mode)
+  (prog-mode . company-mode)
   :config
   (setq company-selection-wrap-around t
 	company-global-modes my/programming-modes))
 
 (use-package flycheck
   :hook
-  (prog-mode . global-flycheck-mode)
+  (prog-mode . flycheck-mode)
   :config
   (setq flycheck-check-syntax-automatically '(save mode-enabled)
 	flycheck-global-modes my/programming-modes))
 
 (use-package ivy-xref
+  :defer t
   :init
   (setq xref-show-definitions-function #'ivy-xref-show-defs)
   (setq xref-show-xrefs-function #'ivy-xref-show-xrefs)
+  :config
   (ivy-configure 'ivy-xref-show-defs
     :update-fn 'auto)
   (ivy-configure 'ivy-xref-show-xrefs
@@ -305,7 +316,7 @@
 	lsp-enable-on-type-formatting nil))
 
 (use-package dap-mode
-  :after lsp-java
+  :defer t
   :config
   (setq dap-auto-configure-features '(sessions locals controls tooltip))
   (dap-auto-configure-mode))
@@ -316,21 +327,17 @@
   :config
   (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
   :hook
-  ((c-mode c++-mode) .
-   (lambda () (require 'ccls) (lsp))))
+  ((c-mode c++-mode) . lsp-deferred))
 
 ;;; Java
 
 (use-package lsp-java
   :hook
-  (java-mode . lsp-deferred)
-  :config
-  (require 'dap-java))
+  (java-mode . lsp-deferred))
 
 ;;; Web
 
 (use-package web-mode
-  :diminish
   :hook
   (sgml-mode . web-mode))
 
@@ -350,13 +357,14 @@
 	pythonic-interpreter "python3"))
 
 (use-package company-anaconda
-  :hook
-  (python-mode .
-	       (lambda ()
-		 (add-to-list 'company-backends
-			      '(company-anaconda :with company-capf)))))
+  :defer t)
+
+(add-hook 'python-mode-hook
+	  (lambda () (add-to-list 'company-backends
+				  '(company-anaconda :with company-capf))))
 
 ;;; Racket
+
 (use-package racket-mode
   :defer t)
 
@@ -365,10 +373,9 @@
 (use-package ess
   :defer t
   :config
-  (setq ess-use-flymake nil)
-  :init
-  (require 'ess-r-mode))
+  (setq ess-use-flymake nil))
 
+(setq initial-major-mode 'fundamental-mode) ; do not trigger prog-mode-hook
 
 ;;;; Visual Niceties
 
@@ -380,28 +387,23 @@
 (set-fontset-font t 'symbol "Noto Color Emoji" nil 'append)
 
 (setq display-time-default-load-average nil
-      display-time-format (format-time-string " %I:%M %p "))
-(add-hook 'after-init-hook 'display-time-mode)
-
-(use-package fancy-battery
-  :hook
-  (after-init . fancy-battery-mode)
-  :config
-  (setq fancy-battery-show-percentage t))
+      display-time-format " | %I:%M %p")
+(display-time-mode)
+(setq battery-mode-line-format "%b%p%% ")
+(setq battery-load-critical 30)
+(display-battery-mode)
 
 (use-package minions
-  :hook
-  (after-init . minions-mode)
-  :config
-  (setq minions-mode-line-lighter "🐢"
+    :config
+    (setq minions-mode-line-lighter "-"
 	minions-mode-line-delimiters nil
-	minions-direct '(flycheck-mode)))
+	minions-direct '(flycheck-mode))
+    (minions-mode))
 
 (use-package moody
-  :hook
-  (after-init . moody-replace-mode-line-buffer-identification)
-  (after-init . moody-replace-vc-mode)
   :config
+  (moody-replace-mode-line-buffer-identification)
+  (moody-replace-vc-mode)
   (setq x-underline-at-descent-line t))
 
 
