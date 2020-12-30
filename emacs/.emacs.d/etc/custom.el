cc726fe24ac9407db8d7d24e02b1a58e991ee9be
icmor <54549777+icmor@users.noreply.github.com>
2020-12-29 23:51:11 -0500

    Misc deletions

--- emacs/.emacs.d/etc/custom.el
+++ emacs/.emacs.d/etc/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(moody minions spacemacs-theme ess racket-mode company-anaconda anaconda-mode emmet-mode web-mode lsp-java ccls dap-mode lsp-mode magit ivy-xref flycheck company ws-butler org-roam org-bullets org-drill org-plus-contrib vterm-toggle vterm pdf-tools ace-window amx flx ivy-hydra counsel ivy elisp-demos helpful which-key gcmh use-package)))
+   '(yasnippet moody minions spacemacs-theme ess racket-mode emmet-mode web-mode lsp-java ccls dap-mode lsp-mode magit ivy-xref flycheck company ws-butler deft org-roam org-bullets org-pomodoro org-plus-contrib vterm-toggle vterm pdf-tools ace-window amx flx ivy-hydra counsel ivy elisp-demos helpful which-key gcmh no-littering use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
