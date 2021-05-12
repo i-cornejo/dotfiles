5034e47b0f5b8c579a9917ea2b789b06b32ec245
icmor <pink@heartofgold.localdomain>
2021-05-12 11:53:13 -0500

    i3blocks scripts

---
+++ i3/.i3/scripts/volume.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$(pamixer --get-mute)" = "false" ]
+then
+    icon="🔈"
+else
+    icon="🔇"
+fi
+
+echo $icon $(pamixer --get-volume)
+
+if [ -n "$button" ]
+then
+	pavucontrol > /dev/null &
+fi
